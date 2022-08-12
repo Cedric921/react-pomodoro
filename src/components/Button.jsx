d9953ec44b9f6097e4f842f9f20ea5e753bd8371
cedric karungu <ckarungu921@gmail.com>
2022-08-12 11:49:45 +0200

    style: add timer style

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = ({ title, activeClass, _cb }) => {
+	return (
+		<button className={activeClass} onClick={_cb}>
+			{title}
+		</button>
+	);
+};
+
+export default Button;
---
+++ src/components/SetPomodoro.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import Button from './Button';
+
+const SetPomodoro = () => {
+	const [newTimer, setNewTimer] = useState({
+		work: 0.3,
+		short: 0.2,
+		long: 1,
+		active: 'work',
+	});
+
+	const handleChange = (input) => {
+		const { name, value } = input.target;
+		switch (name) {
+			case 'work':
+				setNewTimer({ ...newTimer, work: +value });
+				break;
+			case 'shortBreak':
+				setNewTimer({ ...newTimer, short: +value });
+				break;
+			case 'longBreak':
+				setNewTimer({ ...newTimer, long: +value });
+				break;
+			default:
+				break;
+		}
+		console.log(newTimer);
+	};
+
+	const handleSubmit = () => {};
+
+	return (
+		<div className='form-container'>
+			<form noValidate>
+				<div className='input-wrapper'>
+					<input
+						type='text'
+						className='input'
+						name='work'
+						onChange={handleChange}
+						value={newTimer.work}
+					/>
+					<input
+						type='text'
+						className='input'
+						name='shortBreak'
+						onChange={handleChange}
+						value={newTimer.short}
+					/>
+					<input
+						type='text'
+						className='input'
+						name='longBreak'
+						onChange={handleChange}
+						value={newTimer.long}
+					/>
+				</div>
+				<Button title='Set timer' _cb={handleSubmit} />
+			</form>
+		</div>
+	);
+};
+
+export default SetPomodoro;
